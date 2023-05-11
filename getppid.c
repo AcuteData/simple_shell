8dbad5d62248a18c470b45b176ff109d11e5a877
Gloria-Mwamidi <wakilogloria@gmail.com>
2023-05-10 20:44:05 -0700

    Building source fies with the concept page

---
+++ getppid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	pid_t ppid;
+	pid_t pid;
+
+	ppid = getppid();
+	pid = getpid();
+
+	printf(" The parent process ID%u\n", ppid);
+	printf("The current process ID%u\n",pid);
+	return (0);`
+}
