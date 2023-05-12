58ea1f0146096d5bb4deae454df29fe8bc323e4f
ChrisAdongo <jakenyachris@gmail.com>
2023-05-12 02:34:46 -0700

    handle_sig.c

---
+++ handle_sig.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * sigint_sig - handles signal interrupt.
+ * @sig: The signal number to handle.
+ *
+ * Return: void.
+ */
+void sigint_sig(int sig)
+{
+	if (sig == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		_prompt();
+		fflush(stdout);
+	}
+}
